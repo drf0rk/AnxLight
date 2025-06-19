becb681d099ca4c19596274f79af40b436356a15
drf0rk <wolfparade@hotmail.com>
2025-06-19 14:42:41 +1200

    Feat: Create central version control file

---
+++ scripts/anxlight_version.py
@@ -0,0 +1,17 @@
+# AnxLight Version Control
+# This file centralizes version numbers for all key components of the AnxLight project.
+
+# Main Application Versions
+LAUNCHER_VERSION = "0.0.5"
+MAIN_APP_VERSION = "0.0.9"
+
+# Backend Script Versions
+DOWNLOADER_VERSION = "1.0.0"
+LAUNCH_SCRIPT_VERSION = "1.1.0"
+
+# UI-specific Script Versions
+A1111_UI_VERSION = "1.0.6"
+
+# To be used in scripts like:
+# from anxlight_version import LAUNCHER_VERSION
+# print(f"--- AnxLight Launcher v{LAUNCHER_VERSION} ---")
