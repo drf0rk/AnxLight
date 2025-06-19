a4fb16cc1ccd32a8285cfc7c013417867f5a8af5
drf0rk <wolfparade@hotmail.com>
2025-06-19 23:24:13 +1200

    Update MAIN_GRADIO_APP_VERSION to 1.0.0 in anxlight_version.py post-refactor

--- scripts/anxlight_version.py
+++ scripts/anxlight_version.py
@@ -3,12 +3,12 @@
 
 # Launcher and Core App
 ANXLIGHT_LAUNCHER_NOTEBOOK_VERSION = "0.0.5"  # Based on latest in repo and Plan.md v3
-MAIN_GRADIO_APP_VERSION = "0.0.7"          # Current version from Overview.md, will be updated as it's refactored for v3
+MAIN_GRADIO_APP_VERSION = "1.0.0"          # Updated for v3 architecture refactor
 PRE_FLIGHT_SETUP_PY_VERSION = "0.1.0"      # Initial version for this new script
 
 # Backend Scripts (versions to be confirmed/updated as they are touched)
-# Example: DOWNLOADER_EN_PY_VERSION = "0.0.2" # As per Overview.md
-# Example: LAUNCH_PY_VERSION = "0.0.0" # Inherited, may get specific version if modified
+# Example: DOWNLOADER_EN_PY_VERSION = "0.0.2" # Role changed, version TBD
+LAUNCH_PY_VERSION = "0.0.0" # Inherited, may get specific version if modified
 
 # Add other key script versions here as they are developed or modified.
 # For example, versions for scripts in scripts/UIs/ can be added.
