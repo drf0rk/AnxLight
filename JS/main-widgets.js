0c0f0850d17d285b41adfe38f51e867a3abab065
drf0rk <wolfparade@hotmail.com>
2025-06-28 16:45:40 +1200

    Delete obsolete files for Project Trinity cleanup

--- JS/main-widgets.js
+++
@@ -1,9 +0,0 @@
-function toggleContainer() {
-    const downloadContainer = document.querySelector('.container_cdl');
-    const info = document.querySelector('.info');
-    const emp = document.querySelector('.empowerment');
-
-    downloadContainer.classList.toggle('expanded');
-    info.classList.toggle('showed');
-    emp.classList.toggle('showed');
-}
